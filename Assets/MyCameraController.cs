cf9a63639eedb2407fc90928f68857b0b5f8340b
Yuki Nishimura <yukinkopen@gmail.com>
2022-01-18 21:57:19 +0900

    Lesson6 4.12 まで実装

---
+++ Assets/MyCameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour
+{
+    private GameObject unitychan;
+    private float difference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.unitychan = GameObject.Find("unitychan");
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+    }
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+    private Animator myAnimator;
+    private Rigidbody myRigidbody;
+    private float velocityZ = 16f;
+    private float velocityY = 10f;
+    private float velocityX = 10f;
+    private float movableRange = 3.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myAnimator = GetComponent<Animator>();
+        this.myAnimator.SetFloat("Speed", 1);
+        this.myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputVelocityX = 0;
+        float inputVelocityY = 0;
+        if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x)
+        {
+            inputVelocityX = -this.velocityX;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange)
+        {
+            inputVelocityX = this.velocityX;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        {
+            this.myAnimator.SetBool("Jump", true);
+            inputVelocityY = this.velocityY;
+        }
+        else
+        {
+            inputVelocityY = this.myRigidbody.velocity.y;
+        }
+
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+
+        this.myRigidbody.velocity = new Vector3(inputVelocityX, inputVelocityY, velocityZ);
+    }
+}
