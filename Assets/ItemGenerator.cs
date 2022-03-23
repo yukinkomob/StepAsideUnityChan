b51dd4942ba5c0ad875a68e3191df7d3c8f5d9ca
Yuki Nishimura <yukinkopen@gmail.com>
2022-03-23 17:33:27 +0900

    Lesson6 発展課題の実装
    - プレーヤーの50m先のオブジェクトを随時生成

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -10,6 +10,9 @@ public class ItemGenerator : MonoBehaviour
     public GameObject unityChan;
     private int startPos = 80;
     private int goalPos = 360;
+    private int appearanceBoundary = 50;
+    private int sectionDistance = 15;
+    private int targetPos;
     private float posRange = 3.4f;
     private List<GameObject> carPrefabs = new List<GameObject>();
     private List<GameObject> coinPrefabs = new List<GameObject>();
@@ -18,53 +21,64 @@ public class ItemGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = startPos; i < goalPos; i+=15)
-        {
-            int num = Random.Range(1, 11);
-            if (num <= 2)
-            {
-                for (float j = -1; j <= 1; j += 0.4f)
-                {
-                    GameObject cone = Instantiate(conePrefab);
-                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
-                    conePrefabs.Add(cone);
-                }
-            }
-            else
-            {
-                for (int j = -1; j <= 1; j++)
-                {
-                    int item = Random.Range(1, 11);
-                    int offsetZ = Random.Range(-5, 6);
-                    if (1 <= item && item <= 6)
-                    {
-                        GameObject coin = Instantiate(coinPrefab);
-                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetZ);
-                        coinPrefabs.Add(coin);
-                    }
-                    else if (7 <= item && item <= 9)
-                    {
-                        GameObject car = Instantiate(carPrefab);
-                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
-                        carPrefabs.Add(car);
-                    }
-                }
-            }
-        }
+        targetPos = startPos;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float baseZ = unityChan.transform.position.z;
+        if (baseZ > targetPos - appearanceBoundary && targetPos < goalPos)
+        {
+            createObjects(targetPos);
+            targetPos += sectionDistance;
+        }
+
         destroyObsoleteObjects(conePrefabs);
         destroyObsoleteObjects(coinPrefabs);
         destroyObsoleteObjects(carPrefabs);
+    }
+
+    void createObjects(int pos)
+    {
+        int i = pos;
+        int num = Random.Range(1, 11);
+        if (num <= 2)
+        {
+            for (float j = -1; j <= 1; j += 0.4f)
+            {
+                GameObject cone = Instantiate(conePrefab);
+                cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
+                conePrefabs.Add(cone);
+            }
+        }
+        else
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                int item = Random.Range(1, 11);
+                int offsetZ = Random.Range(-5, 6);
+                if (1 <= item && item <= 6)
+                {
+                    GameObject coin = Instantiate(coinPrefab);
+                    coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetZ);
+                    coinPrefabs.Add(coin);
+                }
+                else if (7 <= item && item <= 9)
+                {
+                    GameObject car = Instantiate(carPrefab);
+                    car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
+                    carPrefabs.Add(car);
+                }
+            }
+        }
     }
 
     void destroyObsoleteObjects(List<GameObject> objects)
     {
         int eraseBoundaryZ = 10;
         float baseZ = unityChan.transform.position.z;
+
         for (int i = 0; i < objects.Count; i++)
         {
             if (objects[i] == null)
